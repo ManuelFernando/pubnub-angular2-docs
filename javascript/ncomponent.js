ed0e9c4bb40b20ae72f4302c459d49fd52bbfa30
manuelfernando <manuelcamaron@hotmail.com>
2017-01-26 10:27:17 -0500

    Add ngcomponent for javascript

---
+++ javascript/ncomponent.js
@@ -0,0 +1,10 @@
+(function (app) {
+    app.main_component = ng.core.Component({
+        selector: 'main-component',
+        templateUrl: 'app/template.html',
+        providers: [PubNubAngular]
+    }).Class({
+        constructor: function () {}
+    });
+
+})(window.app || (window.app = {}));
