f8a75f0a4e25c0c83c4b5e6caaafc14c1de731a0
manuelfernando <manuelcamaron@hotmail.com>
2017-01-26 19:15:37 -0500

    Remove spaces

--- typescript/module.ts
+++ typescript/module.ts
@@ -1,14 +1,14 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { PubNubAngular } from 'pubnub-angular2';
-import { appComponent }  from './appComponent';
+import { appComponent } from './appComponent';
 @NgModule({
-    imports:      [BrowserModule],
-    declarations: [appComponent],
-    providers: [ PubNubAngular ],
-    bootstrap:    [ appComponent ]
+    imports:[ BrowserModule ],
+    declarations:[ appComponent ],
+    providers:[ PubNubAngular ],
+    bootstrap:[ appComponent ]
 })
 export class appModule {
-    constructor(){
+    constructor() {
     }
 }
