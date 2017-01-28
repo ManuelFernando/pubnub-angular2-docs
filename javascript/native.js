dbef0b502ff2a41520ff5c05572e2c067f40fb86
manuelfernando <manuelcamaron@hotmail.com>
2017-01-27 20:21:26 -0500

    Add native sdk

---
+++ javascript/native.js
@@ -0,0 +1,35 @@
+var defaultInstance = new PubNub({
+    publishKey: 'YOUR PUB_KEY',
+    subscribeKey: 'ANOTHER SUB_KEY'
+});
+
+var anotherInstance = new PubNub({
+    publishKey: 'ANOTHER PUB_KEY',
+    subscribeKey: 'ANOTHER SUB_KEY'
+});
+
+defaultInstance.publish(
+    {
+        message: {such: 'Hello!'},
+        channel: 'myChannel'
+    },
+    function (status, response) {
+        if (status.error) {
+            console.log(status)
+        } else {
+            console.log("message Published w/ timetoken", response.timetoken)
+        }
+    }
+);
+
+anotherInstance.grant(
+    {
+        channels: ['my_channel'],
+        authKeys: ['my_authkey'],
+        read: true,
+        write: false
+    },
+    function (status) {
+        // handle state setting response
+    }
+);
