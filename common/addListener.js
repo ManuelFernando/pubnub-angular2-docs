545f21f069d8276ce5d5e28d45dd63ef7c6779a0
manuelfernando <manuelcamaron@hotmail.com>
2017-01-31 18:58:40 -0500

    Add example to add a listener

---
+++ common/addListener.js
@@ -0,0 +1,18 @@
+pubnub.addListener({
+  status: function(st) {
+    if (st.category === "PNUnknownCategory") {
+      var newState = {new: 'error'};
+      pubnub.setState({
+        state: newState},
+        function (status) {
+          console.log(st.errorData.message);
+        }
+      );
+    }
+  },
+  message: function(message) {
+    console.log(message);
+  }
+});
+
+pubnub.subscribe({channels: ['myChannel']});
