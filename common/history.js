416d2114f44f876c5f263dd89ff1e6dbc3ef8396
manuelfernando <manuelcamaron@hotmail.com>
2017-01-31 20:15:18 -0500

    Add example to get messages from history

---
+++ common/history.js
@@ -0,0 +1,9 @@
+pubnub.history({
+  channel: 'myChannel1',
+  reverse: false, // false is the default
+  count: 100, // 100 is the default
+  stringifiedTimeToken: true // false is the default
+},
+function (status, response) {
+  console.log(response);
+});
