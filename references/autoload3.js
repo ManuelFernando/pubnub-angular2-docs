6c35c1f19f207588d6f90546135b4d1dd17f2fb3
manuelfernando <manuelcamaron@hotmail.com>
2017-02-06 19:40:41 -0500

    Update code

--- references/autoload3.js
+++ references/autoload3.js
@@ -5,12 +5,12 @@ pubnub.subscribe({
   autoload: 100
 });
 
-var messages = pubnub.getMessage('myChannel1', function(msg){
+var messages = pubnub.getMessage('myChannel1', function(message){
   if (!message) {
     // The history has been loaded.
     console.log(messages);
   } else {
     // It has been received a realtime message.
-    console.log(msg);
+    console.log(message);
   }
 });
