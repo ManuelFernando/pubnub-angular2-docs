45633ca2e81b8b1417cb05fe574b5ed344ec5545
manuelfernando <manuelcamaron@hotmail.com>
2017-02-13 10:50:20 -0500

    Update code

--- references/decrypt.js
+++ references/decrypt.js
@@ -1,1 +1,1 @@
-var data = pubnub.decrypt(data: Object, customCipherKey: ?string);
+var result = pubnub.decrypt(data: Object, customCipherKey: ?string);
--- references/encrypt.js
+++ references/encrypt.js
@@ -1,1 +1,1 @@
-var data = pubnub.encrypt(data: string, customCipherKey: ?string);
+var result = pubnub.encrypt(data: string, customCipherKey: ?string);
