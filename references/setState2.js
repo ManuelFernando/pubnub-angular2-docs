84317639cdff96ddb780f0b95726dbe269e32c1e
manuelfernando <manuelcamaron@hotmail.com>
2017-02-17 12:24:36 -0500

    Add example of state-change

---
+++ references/setState2.js
@@ -0,0 +1,10 @@
+pubnub.setState({
+  state: {
+    "full_name" : "James Patrick Page"
+  },
+  uuid: "jbonham",
+  channels: ['my_channel']
+},
+function (status) {
+  // handle state setting response
+});
