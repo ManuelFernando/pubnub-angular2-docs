8e38a763afc0ec1f7425ad4267a473d9adf8c388
manuelfernando <manuelcamaron@hotmail.com>
2017-02-21 18:46:47 -0500

    Add example of receipts for associated devices

---
+++ others/receipt_device4.js
@@ -0,0 +1,20 @@
+{
+  "pn_mpns": {
+    "type" : "toast",
+    "text1" : "Stock Trade Activity",
+    "text2" : "100 XYZ Stock @ $ 60 BUY is being ordered. Your order is in process. If you did not order, please call helpdesk."
+  },
+  "snapshot": {
+    "last": 40.52,
+    "volume": "3495345",
+    "change": ["+4.02","+7.45%"]
+  },
+  "ticker": {
+    "newsflash": [
+      "Fitchwitz antivirals in the total number of shares being sold in the public offering have exercised in full.",
+      "The offering price of all their option to purchase an additional zillion shares of its common stock is sweet.",
+      "Offering is expected to occur today announced public offering the offering when 100 shares went byebye."
+    ],
+    "url": "http://FitchwitzTech.com/fooz"
+  }
+}
