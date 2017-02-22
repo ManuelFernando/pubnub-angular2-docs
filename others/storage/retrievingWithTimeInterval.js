c439e803a0d1e0e5613e22a9b6b53428288d22ff
manuelfernando <manuelcamaron@hotmail.com>
2017-02-22 17:48:49 -0500

    Add example of retrieving from storage using time interval

---
+++ others/storage/retrievingWithTimeInterval.js
@@ -0,0 +1,9 @@
+pubnub.history({
+  channel: 'history_channel',
+  count: 100,
+  start: 13827485876355504,
+  end: 13827475876355504
+},
+function (status, response) {
+  // handle status, response
+});
